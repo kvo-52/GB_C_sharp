81a7196ed99a0b65070908a6a7e05b45a0f887a8
kvo.52@mail.ru <kvo.52@mail.ru>
2022-10-19 22:26:28 +0300

    without second number

---
+++ Seminar-2-2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 2: Напишите программу, которая выводит случайное трёхзначное число 
+// и удаляет вторую цифру этого числа. Не использовать строки для расчета.
+
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int Number = new Random().Next(100, 1000);
+int NewNumber = 0;
+
+NumberWithoutSecond();
+System.Console.WriteLine();
+System.Console.WriteLine($"Случайное трехзначное число {Number}");
+System.Console.WriteLine($"Если из числа {Number} удалить вторую цифру, получится число {NewNumber}");
+System.Console.WriteLine();
+
+int NumberWithoutSecond()
+{
+    NewNumber = Number / 100 * 10 + Number % 10;
+    return NewNumber;
+}
