d9b1f5da5d41e67891976f9b85cd75a971c83afa
kvo.52@mail.ru <kvo.52@mail.ru>
2022-10-20 21:58:16 +0300

    Seminar 3

--- Seminar-3-6/Program.cs
+++ Seminar-3-6/Program.cs
@@ -1,1 +1,30 @@
-﻿
+﻿//   Напишите программу, которая генерирует последовательность случайных чисел до тех пор,
+// пока не сгенерирует кратное число введенному в начале.
+
+int Prompt(string msg)
+{
+    System.Console.Write(msg);
+    return int.Parse(Console.ReadLine());
+}
+
+
+void Starter()
+{
+    int userAnswer = Prompt("Введите число от 1 до 10: ");
+
+    while (true)
+    {
+        int num = new Random().Next(1, 11);
+        if (num % userAnswer == 0)
+        {
+            System.Console.WriteLine(num);
+            break;
+        }
+
+        System.Console.WriteLine($"Пока не получилось num={num}");
+    }
+
+}
+
+Starter();
+
