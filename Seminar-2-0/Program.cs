5f43fdbda4dea5fcdf2e6702d26e31902f3b6d85
kvo.52@mail.ru <kvo.52@mail.ru>
2022-10-20 13:57:14 +0300

    Days week

---
+++ Seminar-2-0/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+int numberDay = ValidateMessage("Введите число от 1 до 7: ");
+int day= numberDay;
+
+
+if (day==6 || day==7)
+{
+ Console.WriteLine("Да. Выходной день");   
+}
+if (day>0 && day<6)
+{
+   Console.WriteLine("Нет. Будний день");
+}
+if (day>7 || day<0)
+{
+   Console.WriteLine("Такого дня недели нет"); 
+}
+
+
+int ValidateMessage (string message)
+{
+    System.Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
---
+++ Seminar-2-4/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+int numberDay = ValidateMessage("Введите число от 1 до 7: ");
+int day= numberDay;
+
+if (day==6 || day==7)
+{
+ Console.WriteLine("Да. Выходной день");   
+}
+else 
+{
+    Console.WriteLine("Нет. Будний день");
+}
+
+int ValidateMessage (string message)
+{
+    System.Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
