f09c6c93d64f49fcb3167203a24cd3a2bb0533b0
kvo.52@mail.ru <kvo.52@mail.ru>
2022-10-20 21:39:21 +0300

    Seminar 3

---
+++ Seminar-3-3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек
+//  и находит расстояние между ними в 2D пространстве.
+// A (3,6); B (2,1) -> 5,09 
+// A (7,-5); B (1,-1) -> 7,21
+
+
+int Prompt(string message)
+{
+    System.Console.Write(message);   //выводит сообщение в консоли
+    return int.Parse(Console.ReadLine());   // считать строку с консоли и преобразовать в целое значение
+}
+int x1=Prompt("Введите x1:");
+int y1=Prompt("Введите y1:");
+int x2=Prompt("Введите x2:");
+int y2=Prompt("Введите y2:");
+
+int x= x1-x2;
+int y=y1-y2;
+double Lenght= Math.Sqrt(x*x+y*y); // возводим в квадрат
+Console.WriteLine($"{Lenght:f2}");
---
+++ Seminar-3-4/Program.cs
@@ -0,0 +1,21 @@
+﻿/  Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+int Prompt(string message)
+{
+    System.Console.Write(message);   //выводит сообщение в консоли
+    return int.Parse(Console.ReadLine());   // считать строку с консоли и преобразовать в целое значение
+}
+int N =Prompt("Введите число больше 0:");
+int count=1;  // счетчик
+while (count<=N)  //условие
+{
+    System.Console.WriteLine(count*count); //в квадрат возводим
+    count++; // увеличиваем счетчик
+}
+for (int i = 1; i<=N; i++)  // цикл for
+{
+   System.Console.WriteLine(Math.Pow(i,2));
+}
