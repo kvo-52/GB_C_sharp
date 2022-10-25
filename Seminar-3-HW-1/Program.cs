8f572c2ba26a0c11b94fe1a0fe8d3725acc9c124
kvo.52@mail.ru <kvo.52@mail.ru>
2022-10-25 21:20:26 +0300

    Home Work for seminar-3

---
+++ Seminar-3-HW-1/Program.cs
@@ -0,0 +1,59 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет,
+// является ли оно палиндромом. Без использования строк
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+
+int ValidateMessage (string message)
+{
+    System.Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int FirstNumber(int a)
+{
+    int result = ((a / 10000) % 10);
+    return result;
+}
+int SecondNumber(int a)
+{
+    int result = ((a / 1000) % 10);
+    return result;
+}
+int FourthNumber(int a)
+{
+    int result = ((a / 10) % 10);
+    return result;
+}
+int FifthtNumber(int a)
+{
+    int result = ((a / 1) % 10);
+    return result;
+}
+
+
+int value= ValidateMessage("Введите пятизначное число: ");
+int firstvalue= FirstNumber(value);
+int secondtvalue= SecondNumber(value);
+int fourthvalue= FourthNumber(value);
+int fifthvalue= FifthtNumber(value);
+
+if (value>9999 && value<100000)
+{
+    if (firstvalue==fifthvalue && secondtvalue==fourthvalue)
+{
+   Console.WriteLine($"Число {value} является полиндромом");
+}
+else
+{
+    Console.WriteLine($"Число {value} не является полиндромом");
+}  
+}
+else
+{
+ Console.WriteLine("НЕВЕРНО! Необходимо ввести пятизначное число.");   
+}
+
+
