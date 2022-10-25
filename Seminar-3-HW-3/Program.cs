1cd334e6d62acafb6d6f4806d0856985e419dd2e
kvo.52@mail.ru <kvo.52@mail.ru>
2022-10-25 21:31:02 +0300

    Home Work for seminar-3

---
+++ Seminar-3-HW-3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+
+int Prompt(string message)
+{
+    System.Console.Write(message);   
+    return int.Parse(Console.ReadLine());   
+}
+int N =Prompt("Введите число больше 0:");
+int count=1;  // счетчик
+while (count<=N)  //условие
+{
+    System.Console.WriteLine(count*count*count); //в куб возводим
+    count++; // увеличиваем счетчик
+}
+// for (int i = 1; i<=N; i++)  
+// {
+//    System.Console.WriteLine(Math.Pow(i,3));
+// }
