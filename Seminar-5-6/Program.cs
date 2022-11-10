9821e5f751b0bc6fb56ac7ef3cf88305b3b2a704
kvo.52@mail.ru <kvo.52@mail.ru>
2022-11-10 21:59:50 +0300

    5 seminar

---
+++ Seminar-5-6/Program.cs
@@ -0,0 +1,23 @@
+﻿//  Создать массив и посчитать среднее значение
+
+double Average(int[] arr)
+{
+    double total = 0;
+    foreach (int item in arr)
+    {
+        total += item;
+    }
+
+    return total / arr.Length;
+}
+
+double MiddleSquare(int[] arr)
+{
+    double total = 0;
+    foreach (var item in arr)
+    {
+        total += Math.Pow(item, 2);
+    }
+    return Math.Sqrt(total / arr.Length);
+}
+
