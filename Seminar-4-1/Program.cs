8d81b57d27627db334a081357e75835d21ec8c0a
kvo.52@mail.ru <kvo.52@mail.ru>
2022-11-10 16:03:02 +0300

    4 seminar

---
+++ Seminar-4-1/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход число (А)
+//  и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+bool ValidateNumber(int number)
+{
+    if ( number<=0)
+    {
+        System.Console.WriteLine("Число должно быть положительным");
+        return false;
+    }
+    return true;
+}
+
+int SumNambers(int number)
+{
+    int sum=0;
+    for (int i=1; i<=number; i++)
+    {
+        sum+=i;
+    }
+    return sum;
+}
+
+int number = Prompt("Введите число");
+if (ValidateNumber(number))
+{
+    int sum =SumNambers(number);
+    System.Console.WriteLine($"Суума чисел от 1 до {number} до {sum}");
+}
