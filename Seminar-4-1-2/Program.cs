4fb3c375de4b79aaae96d3f919ed5bdbf4728f63
kvo.52@mail.ru <kvo.52@mail.ru>
2022-11-10 16:49:09 +0300

    4 seminar

---
+++ Seminar-4-1-2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход число и
+//  выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+int NumberDigit(int UserAnswer)
+{
+    int count=0;
+    while(UserAnswer>=1)
+    {
+        UserAnswer=UserAnswer/10;
+        count++;
+    }
+    return count;
+}
+
+int number = Prompt("Введите число: ");
+int count=NumberDigit(number);
+Console.WriteLine($"Количество чисел {count}");
