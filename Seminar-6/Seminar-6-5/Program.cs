3023e16543fe94546421a18c8470d8405730a446
kvo.52@mail.ru <kvo.52@mail.ru>
2022-11-23 20:08:50 +0300

    HW-1 for 8 seminar

--- Seminar-6/Seminar-6-5/Program.cs
+++ Seminar-6/Seminar-6-5/Program.cs
@@ -1,6 +1,30 @@
 ﻿// Напишите программу, которая будет создавать копию заданного 
 // массива с помощью поэлементного копирования.
 
+void PrintArray(int[] array)
+{
+    foreach (int item in array)
+    {
+        System.Console.Write($"{item} ");
+    }
+    System.Console.WriteLine();
+}
+
+int[] CreateArray(int temp)
+{
+    if(temp <2)
+    {
+        temp = 2;
+    }
+    int[] tempArray = new int[temp];
+    tempArray[0] = 0;
+    tempArray[1] = 1;
+    for (int i = 2; i < temp; i++)
+    {
+        tempArray[i] = tempArray[i-1] + tempArray[i-2];
+    }
+    return tempArray;
+}
 int[] CopyArr(int[] arr)
 {
     int[] result = new int[arr.Length];
---
+++ Seminar-8/Seminar-8-HW-1/Program.cs
@@ -0,0 +1,73 @@
+﻿// Задача 1: Задайте двумерный массив. Напишите программу, 
+// которая упорядочит по убыванию элементы каждой строки 
+// двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int[,] CreateRandomArrayOfInt(int rows, int cols, int min, int max) // Создает двумерный массив с заданой размерностью и границами генерации целых чисел
+{
+    int[,] temp = new int[rows, cols];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            temp[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return temp;
+}
+
+void PrintArrayOfInt(int[,] array) // Печать двумерного массива целых чисел
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        System.Console.Write($"{array[i, 0]}\t");
+        for (int j = 1; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void ArrangeElementArray(int[,] array) // упорядочит по убыванию элементы
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+void Main()
+{
+    System.Console.Clear();
+    const int rowsInArray = 4;
+    const int colsInArray = 4;
+    const int minOfRandom = -5;
+    const int maxOfRandom = 5;
+    int[,] arrayOfInt = CreateRandomArrayOfInt(rowsInArray, colsInArray, minOfRandom, maxOfRandom);
+    PrintArrayOfInt(arrayOfInt);
+    System.Console.WriteLine();
+    ArrangeElementArray(arrayOfInt);
+    PrintArrayOfInt(arrayOfInt);
+    
+}
+
+Main();               
