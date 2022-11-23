61ccfa325e9b3345b6165dbe6cf9c73cbe18cee9
kvo.52@mail.ru <kvo.52@mail.ru>
2022-11-23 20:33:14 +0300

    HW-2 for 8 seminar

---
+++ Seminar-8/Seminar-8-HW-2/Program.cs
@@ -0,0 +1,75 @@
+﻿// Задача 2: Задайте прямоугольный двумерный массив.
+//Напишите программу,  которая будет 
+// находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки
+//  с наименьшей суммой элементов: 1 строка
+
+int[,] CreateRandomArrayOfInt(int rows, int cols, int min, int max) // Создает двумерный массив и границами генерации целых чисел
+{
+    int[,] temp = new int[rows, cols];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            temp[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return temp;
+}
+
+void PrintArrayOfInt(int[,] array) // Печать двумерного массива целых чисел
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        System.Console.Write($"{array[i, 0]}\t");
+        for (int j = 1; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void SumMinRowsArray(int[,] array)
+{
+    int minRow = 0;
+    int minSumRow = 0;
+    int sumRow = 0;
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        minRow += array[0, i];
+    }
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++) sumRow += array[i, j];
+        if (sumRow < minRow)
+        {
+            minRow = sumRow;
+            minSumRow = i;
+        }
+        sumRow = 0;
+    }
+    System.Console.WriteLine($"Номер строки с наименьшей суммой элементов: {minSumRow + 1} строка");
+}
+
+void Main()
+{
+    System.Console.Clear();
+    const int rowsInArray = 5;
+    const int colsInArray = 4;
+    const int minOfRandom = 0;
+    const int maxOfRandom = 10;
+    int[,] arrayOfInt = CreateRandomArrayOfInt(rowsInArray, colsInArray, minOfRandom, maxOfRandom);
+    PrintArrayOfInt(arrayOfInt);
+    System.Console.WriteLine();
+    SumMinRowsArray(arrayOfInt);
+    System.Console.WriteLine();
+
+}
+
+Main();
