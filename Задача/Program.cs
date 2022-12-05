836d5d47e72db00fb2f9c1bc9d79bf76492ae200
kvo.52@mail.ru <kvo.52@mail.ru>
2022-12-05 19:31:39 +0300

    add metod PrintArray

---
+++ Задача/Program.cs
@@ -0,0 +1,30 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк,
+//  длина которых меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, 
+//  либо задать на старте выполнения алгоритма.
+//  При решение не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+
+void SecondArrayString(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+string stringStart = Console.ReadLine();
+string[] stingArray = stringStart.Split();
+
